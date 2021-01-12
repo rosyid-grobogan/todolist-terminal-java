1ec3ca414919af778877d4cdf1f98a47540b29a5
Rosyid <rosyidgrobogan@gmail.com>
2021-01-12 07:37:25 +0700

    test input data in terminal

---
+++ src/test/TodoListControllerTest.java
@@ -0,0 +1,16 @@
+package test;
+import static com.rosyidgrobogan.controller.TodoListController.*;
+public class TodoListControllerTest {
+    public static void main(String[] args) {
+        // test
+        testInput();
+    }
+
+    public static void testInput(){
+        String name = input("Nama");
+        System.out.println("Hi "+ name);
+
+        String hobby = input("Hobi");
+        System.out.println("Saya suka " + hobby);
+    }
+}
